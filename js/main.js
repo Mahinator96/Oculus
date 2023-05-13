ea64ababe2ad00212db19eb6e390957b5f4e9cf0
LiderMahinator <pomnimenia2019@gmail.com>
2023-05-13 16:11:17 +0300

    Готовы секции features и games

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-$(function(){
+'use strict'
 
+// Работа с табами
+const tabItems = document.querySelectorAll('.tabs__btn-item');
+const tabContents = document.querySelectorAll('.tabs__content-item');
+
+// Ф-ия показа слайда
+const open = (e) => {
+	const tabTarget = e.currentTarget;
+	const buttonID = document.querySelector(`#${tabTarget.dataset.button}`);
+
+	// Убрать активный класс у всех табов
+	tabItems.forEach(function(item) {
+		item.classList.remove('tabs__btn-item--active');
+	})
+	// Спрятать все слайды
+	tabContents.forEach(function(item) {
+		item.classList.remove('tabs__content-item--active');
+	})
+	// Добавить табу активный класс
+	tabTarget.classList.add('tabs__btn-item--active');
+	// Показать слайд
+	buttonID.classList.add('tabs__content-item--active');
+}
+
+// Каждому табу повесить обработчик 'click'
+tabItems.forEach(function(tabItem) {
+	tabItem.addEventListener('click', open);
 });
